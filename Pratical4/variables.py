c2c1caa142f4a43730e7d5d8c7b8ac7c8d30a9d9
ahang6000 <822274119@qq.com>
2024-03-06 14:58:00 +0800

    combination of running and strength exercises is better

---
+++ Pratical4/variables.py
@@ -0,0 +1,11 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+if d>e:
+   print("only running is better")
+elif d<e:
+   print("combination of running and strength exercise is better")
+else:
+   print("both have same effect")
