1f9412e0ff51cbed23d52a20d515133bee0023e4
ahang6000 <822274119@qq.com>
2024-03-06 15:20:33 +0800

    edit variables.py

--- Pratical4/variables.py
+++ Pratical4/variables.py
@@ -9,3 +9,18 @@ elif d<e:
    print("combination of running and strength exercise is better")
 else:
    print("both have same effect")
+#The combination of running and strength exercise is better
+
+X=True
+Y=False
+if X=True and Y=True:
+	W=False
+elif X= False and Y=False:
+	W=False
+else:
+	W=True
+#Truth table
+#X=True Y=False=> W=True
+#X=True Y=True=> W=False
+#X=False Y=True=> W=True
+#X=False Y=False=> W=False  
